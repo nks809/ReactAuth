67b24db2fb89638a511a3a807a8a59b21c82bdab
Nikhil Singh <nks291997@gmail.com>
2023-07-28 20:14:42 +0530

    updated files

--- auth-using-axios/src/App.js
+++ auth-using-axios/src/App.js
@@ -1,10 +1,10 @@
-import { Login } from './Login';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Outlet/>
     </div>
   );
 }
